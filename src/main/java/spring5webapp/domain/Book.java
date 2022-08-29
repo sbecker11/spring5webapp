cc9a06e33a76ba2b8265cef423b0c4dfe1fa7891
Shawn Becker <sbecker11@me.com>
2022-08-28 20:45:29 -0600

    added BookController and list.html template

--- src/main/java/spring5webapp/domain/Book.java
+++ src/main/java/spring5webapp/domain/Book.java
@@ -80,8 +80,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
